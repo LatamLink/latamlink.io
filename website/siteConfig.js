d6a60a31e886c44ca8ecc3798ef3dacff81fc7bd
JustinCast <justincastillovalladares@gmail.com>
2020-04-02 19:11:46 -0600

    static website added

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -11,51 +11,51 @@
 // List of projects/orgs using your project for the users page.
 const users = [
   {
-    caption: 'User1',
+    caption: "User1",
     // You will need to prepend the image path with your baseUrl
     // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/undraw_open_source.svg',
-    infoLink: 'https://www.facebook.com',
-    pinned: true,
-  },
+    image: "/img/undraw_open_source.svg",
+    infoLink: "https://www.facebook.com",
+    pinned: true
+  }
 ];
 
 const siteConfig = {
-  title: 'LatamLink Testnet', // Title for your website.
-  tagline: 'Documentación Técnica',
-  url: 'http://docs.latamlink.io/', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  title: "LatamLink Testnet", // Title for your website.
+  tagline: "Documentación Técnica",
+  url: "http://docs.latamlink.io/", // Your website URL
+  baseUrl: "/", // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'documentacion',
-  organizationName: 'LatamLink',
+  projectName: "documentacion",
+  organizationName: "LatamLink",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'como-iniciar', label: 'Docs'},
-    {doc: 'api', label: 'APIs'},
-    {page: 'ayuda', label: 'Ayuda'},
-    {blog: true, label: 'Blog'},
+    { doc: "como-iniciar", label: "Docs" },
+    { doc: "api", label: "APIs" },
+    { page: "ayuda", label: "Ayuda" },
+    { blog: true, label: "Blog" }
   ],
 
   // If you have users set above, you add it here:
   users,
 
   /* path to images for header/footer */
-  headerIcon: 'img/latamlink_logo-h-full-color-overwhite.svg',
-  footerIcon: 'img/footerlogo.png',
-  favicon: 'img/favicon.ico',
+  headerIcon: "img/latamlink_logo-h-full-color-overwhite.svg",
+  footerIcon: "img/footerlogo.png",
+  favicon: "img/favicon.ico",
 
   /* Colors for website */
   colors: {
-    primaryColor: '#19af54',
-    secondaryColor: '#000',
+    primaryColor: "#19af54",
+    secondaryColor: "#000"
   },
 
   /* Custom fonts for website */
@@ -71,27 +71,29 @@ const siteConfig = {
     ]
   },
   */
-  editUrl: 'https://github.com/LatamLink/documentacion/edit/master/docs/',
+  editUrl: "https://github.com/LatamLink/documentacion/edit/master/docs/",
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
   copyright: `Copyright © ${new Date().getFullYear()} LatamLink`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'default',
+    theme: "default"
   },
-
+  separateCss: [
+    "css/static_website.css"
+  ],
   // Add custom scripts here that would be placed in <script> tags.
-  scripts: ['https://buttons.github.io/buttons.js'],
+  scripts: ["https://buttons.github.io/buttons.js"],
 
   // On page navigation for the current documentation page.
-  onPageNav: 'separate',
+  onPageNav: "separate",
   // No .html extensions for paths.
   cleanUrl: true,
 
   twitterUsername: "EOSCostaRica",
   // Open Graph and Twitter card images.
-  ogImage: 'img/undraw_online.svg',
-  twitterImage: 'img/undraw_tweetstorm.svg',
+  ogImage: "img/undraw_online.svg",
+  twitterImage: "img/undraw_tweetstorm.svg",
 
   // For sites with a sizable amount of content, set collapsible to true.
   // Expand/collapse the links and subcategories under categories.
@@ -105,7 +107,7 @@ const siteConfig = {
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
-    repoUrl: 'https://github.com/LatamLink',
+  repoUrl: "https://github.com/LatamLink"
 };
 
 module.exports = siteConfig;
