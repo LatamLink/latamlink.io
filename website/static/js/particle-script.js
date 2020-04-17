cf4eecaa1dc7767cccc12e6706a898014e148494
JustinCast <justincastillovalladares@gmail.com>
2020-04-17 15:00:00 -0600

    feat: i18n enabled on home page close #45

--- website/static/js/particle-script.js
+++ website/static/js/particle-script.js
@@ -1,12 +1,32 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load("particles-js", "other/particles.json", function() {});
-$(document).ready(function() {
-  $("#more").click(function() {
+particlesJS.load("particles-js", "other/particles.json", function () {});
+$(document).ready(function () {
+  $("#more").click(function () {
     $("html,body").animate(
       {
-        scrollTop: $(".content").offset().top
+        scrollTop: $(".content").offset().top,
       },
       "slow"
     );
   });
+
+  var i18n = $.i18n({
+    locale: "en",
+  });
+
+  // i18n configuration
+  var language = "en";
+  var path = "../../jquery.i18n/languages/" + language + ".json";
+  i18n.load(path, language).done(() => $("html").i18n());
+
+  $(".language").change(function () {
+    language = $(".language option:selected").val();
+    i18n.locale = language;
+    path = `../../jquery.i18n/languages/${language}.json`;
+    console.log(language);
+    i18n.load(path, language).done(() => $("html").i18n());
+  });
+
+  // Enable debug
+  $.i18n.debug = true;
 });
