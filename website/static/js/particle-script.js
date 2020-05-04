09ba8fd3c679b8d644c8c47a891d4f57e79dd2dc
JustinCast <justincastillovalladares@gmail.com>
2020-05-04 11:44:35 -0600

    refactor: styles refactorized

--- website/static/js/particle-script.js
+++ website/static/js/particle-script.js
@@ -9,22 +9,4 @@ $(document).ready(function () {
       "slow"
     );
   });
-
-  let style = window.getComputedStyle($("div.tools")[0]);
-  switch (style.gridTemplateColumns.split(" ").length) {
-    case 1:
-      $("div.tools > div").removeClass("border-t border-b border-r border-l");
-      $("div.tools > div").removeClass("pt-5 pb-5 pl-5 pr-5");
-      break;
-    case 2:
-      $("div.tools > div").removeClass("border-t");
-      $("div.tools > div").removeClass("border-b");
-      $("div.tools > div").removeClass("border-r");
-      $("div.tools > div").removeClass("border-l");
-      $("div.tools > div:nth-child(odd)").addClass("border-b border-r");
-      $("div.tools > div:nth-child(even)").addClass("border-b");
-      break;
-    default:
-      break;
-  }
 });
